02cb435d230b4304afcf4570caf49867adbd5a90
parthiv360 <parthivsarkar360@gmail.com>
2022-07-24 16:28:43 +0530

    pushing new files

--- page/views.py
+++ page/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 import requests
 # Create your views here.
 
-def home(request):
-    response= requests.get('https://codeforces.com/api/user.info?handles=parthivsarkar360').json()
-    
-    return render(request, "page/home.html", {'response': response})
 
+def home(request):
+    # response = requests.get(
+    #     'https://codeforces.com/api/user.info?handles=parthivsarkar360').json()
+
+    return render(request, "page/home.html")
